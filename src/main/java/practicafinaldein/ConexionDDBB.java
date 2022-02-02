e83ab4baff63d775bc24ea8df0054b03aafd34d9
Jhoan López <jhoanlopezclase@gmail.com>
2022-02-02 22:59:05 +0100

    Versión 1.0

---
+++ src/main/java/practicafinaldein/ConexionDDBB.java
@@ -0,0 +1,43 @@
+package practicafinaldein;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * @date 2 feb. 2022
+ * @author Jhoan López
+ * @email Jhoanlopezclase@gmail.com
+ */
+
+public class ConexionDDBB {
+    
+    public static void ConectarDBSQLITE () {
+        Properties properties = new Properties();
+        Connection conexion = null;
+        
+        try {
+            properties.load(new FileInputStream(new File("properties.properties")));
+            String url = properties.get("url").toString();
+            try {
+                conexion = DriverManager.getConnection(url); 
+                String guion = "--------------------";
+                System.out.println(guion + " CONECTADO CON LA BASE DE DATOS " + guion);
+            } catch (SQLException ex) {
+                System.out.print("ERROR CONEXIÓN CON BASE DE DATOS: ");
+                System.out.println(ex);
+            } 
+        } catch (FileNotFoundException ex) {
+            System.out.print("ERROR: ");
+            System.out.println(ex);
+        } catch (IOException ex) {
+            System.out.print("ERROR: ");
+            System.out.println(ex);
+        } 
+    } 
+}
---
+++ src/main/java/practicafinaldein/Main.java
@@ -0,0 +1,14 @@
+package practicafinaldein;
+
+/**
+ * @date 2 feb. 2022
+ * @author Jhoan López
+ * @email Jhoanlopezclase@gmail.com
+ */
+
+public class Main {
+    
+    public static void main (String [] args) {   
+        ConexionDDBB.ConectarDBSQLITE();
+    }
+}
